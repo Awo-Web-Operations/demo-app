db89558e987041f40934d214a299056e5c2cc69f
olasubomi <olasubomi.awokoya@hotmail,com>
2022-04-19 16:03:27 -0700

    Making updates to major files to move task template forward to test/review developer progress

--- src/components/mealsPage/ComponentToPrint.js
+++ src/components/mealsPage/ComponentToPrint.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactToPrint from 'react-to-print';
+import './componentToPrint.css'
 
 class ComponentToPrint extends React.Component {
 
@@ -6,11 +8,15 @@ class ComponentToPrint extends React.Component {
         super(props);
     }
 
+    componentDidMount(){
+      console.log(this)
+    }
+
     render() {
         const { value, mealPrep, ingredientsList, categories, utensilsList } = this.props;
         let displayedCategories = ''
         displayedCategories = categories.map((cat) => cat + ', ');
-        let displayedIngredients = ingredientsList.map((ingredientSyntax) => <li key={ingredientSyntax} >{ingredientSyntax}</li>);
+        let displayedIngredients = ingredientsList.map((ingredientSyntax) => <li key={ingredientSyntax} > <h4>{ingredientSyntax} </h4></li>);
         let displayedUtensils =  utensilsList.map((utensil) => <li key={utensil} >{utensil}</li>);
         //  displayedCategories+= '</div>'
     
@@ -25,7 +31,7 @@ class ComponentToPrint extends React.Component {
         
         return (
             <div>
-               <div className="container">
+               {/* <div className="container">
                 <div className="row" style={{ width: "100%" }}>
                   <div className="detail-firstCol col-md-5 col-sm-12" >
                     <img src={this.props.mealImageData} ></img>
@@ -39,10 +45,10 @@ class ComponentToPrint extends React.Component {
               Ingredients:<br></br>
               <ul>{displayedIngredients}</ul><br></br>
               Utensils Needed:
-              <ul>{displayedUtensils}</ul>
+              <ul>{displayedUtensils}</ul> */}
 
               {/* Page 2 */}
-              <div style={{"pageBreakBefore": "always"}}>
+              {/* <div style={{"pageBreakBefore": "always"}}>
                 <div> 
                   {this.props.instructionChunk1.title}<br></br>
                   {this.props.instructionChunk1.dataName}<br></br>
@@ -69,7 +75,117 @@ class ComponentToPrint extends React.Component {
                 </div>
                 {this.props.tips}
                 <div>Connect with us @chop_soul_full</div>
-              </div>
+              </div> */}
+                <div className="print">
+                  <div className="print_page_1">
+                    <div className="print_page_1_row_1">
+                      <img className="print_main_logo_img" src={this.props.mealImageData} ></img>
+                    </div>
+                    <div className="print_page_1_row_2">
+                      <div className="print_page_1_col_1">
+                        <div className="print_top">
+                          <h2>Meal: {this.props.mealName}</h2>
+                        </div>
+                        <div className="meal_details">
+                          <div className="col_2">
+                            <div className="meal_detail">
+                              <h2>Prep Time:</h2>
+                              <p>{this.props.prepTime}</p>
+                            </div>
+                            <div className="meal_detail">
+                              <h2>Cook Time:</h2>
+                              <p>{this.props.cookTime}</p>
+                            </div>
+                          </div>
+                          <div className="col_2">
+                            <div className="meal_detail">
+                              <h2>Serves:</h2>
+                              <p>{this.props.serves}</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="print_details">
+                          <div className="print_detail">
+                            <h2>Utensils Needed</h2>
+                            <ul>{displayedUtensils}</ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="print_col_1_detail">
+                        <h2>Ingredients</h2>
+                        <ul>{displayedIngredients}</ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="print_page_2">
+                    <div className="print_body">
+                      <div className="meal_instructions">
+                      {(this.props.instructionChunk1.title || this.props.instructionChunk2.title) &&
+                        <div className="meal_instruction"> 
+                        {this.props.instructionChunk1.title &&
+                          <div>
+                            <div className="title"><h4>1</h4>{this.props.instructionChunk1.title}</div>
+                            <div>{this.props.instructionChunk1.dataName}</div>
+                            <div>{this.props.instructionChunk1.instructionSteps}</div>
+                          </div>
+                          }
+                          {this.props.instructionChunk2.title &&
+                          <div>
+                            <div className="title"><h4>2</h4>{this.props.instructionChunk2.title}</div>
+                            <div>{this.props.instructionChunk2.dataName}</div>
+                            <div>{this.props.instructionChunk2.instructionSteps}</div>
+                          </div>
+                          }
+                        </div>
+                        }
+                        {(this.props.instructionChunk3.title || this.props.instructionChunk4.title) &&
+                        <div className="meal_instruction"> 
+                        {this.props.instructionChunk3.title &&
+                          <div>
+                            <div className="title"><h4>3</h4>{this.props.instructionChunk3.title}</div>
+                            <div>{this.props.instructionChunk3.dataName}</div>
+                            <div>{this.props.instructionChunk3.instructionSteps}</div>
+                          </div>
+                          }
+                          {this.props.instructionChunk4.title &&
+                          <div>
+                            <div className="title"><h4>4</h4>{this.props.instructionChunk4.title}</div>
+                            <div>{this.props.instructionChunk4.dataName}</div>
+                            <div>{this.props.instructionChunk4.instructionSteps}</div>
+                          </div>
+                          }
+                        </div>
+                        }
+                        {(this.props.instructionChunk5.title || this.props.instructionChunk6.title) &&
+                        <div className="meal_instruction">
+                        {this.props.instructionChunk5.title && 
+                          <div>
+                            <div className="title"><h4>5</h4>{this.props.instructionChunk5.title}</div>
+                            <div>{this.props.instructionChunk5.dataName}</div>
+                            <div>{this.props.instructionChunk5.instructionSteps}</div>
+                          </div>
+                        }
+                        {this.props.instructionChunk6.title &&
+                          <div>
+                            <div className="title"><h4>6</h4>{this.props.instructionChunk6.title}</div>
+                            <div>{this.props.instructionChunk6.dataName}</div>
+                            <div>{this.props.instructionChunk6.instructionSteps}</div>
+                          </div>
+                        }
+                        </div>}
+                        {this.props.tips}
+                        
+                      </div>
+                    </div>
+                    <div className='tips'>
+                      <h2>Tips:</h2>
+                    </div>
+                      <div className='footer'>
+                        <h2>Connect with us @chop_soul_full</h2>
+                      </div>
+                  </div>
+                  
+                </div>
 
             </div>
         );
